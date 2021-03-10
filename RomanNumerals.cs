2094f0fe3422d3e5bac8d13a76012a32f92ba871
nickprog03@gmail.com <nickprog03@gmail.com>
2021-03-10 17:31:05 +0800

    Добавьте файлы проекта.

---
+++ RomanNumerals.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task
+{
+    class Program
+    {
+        private static readonly Dictionary<char, int> romanDigits = new Dictionary<char, int> {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        public static int RomanToInt(string roman)
+        {
+            int total = 0;
+            char previousChar = roman[0];
+            foreach (char currentChar in roman)
+            {
+                total += romanDigits[currentChar];
+                if (romanDigits[previousChar] < romanDigits[currentChar])
+                {
+                    total -= romanDigits[previousChar] * 2;
+                }
+                previousChar = currentChar;
+            }
+            return total;
+        }
+
+        static void Main(string[] args)
+        {
+            string RomanString = "MMCMLXXXIV";
+            int IntegerNumber = RomanToInt(RomanString);
+            Console.WriteLine(IntegerNumber);
+
+
+            //Console.WriteLine(RomanToInteger("MMCMLXXXIV")); //example
+        }
+    }
+}
