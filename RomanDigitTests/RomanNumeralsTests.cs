69e4db309ea8dc016c6bd320afe33a3f3eb9879a
nickprog03@gmail.com <nickprog03@gmail.com>
2021-03-11 19:40:08 +0800

    publish

---
+++ RomanDigitTests/RomanNumeralsTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanDigit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanDigit.Tests
+{
+    [TestClass()]
+    public class RomanNumeralsTests
+    {
+        [TestMethod()]
+        public void RomanToIntTest()
+        {
+            string s = "CMXCIX";
+            int result = 999;
+
+            int actual = RomanNumerals.RomanToInt(s);
+
+            Assert.AreEqual(result, actual);
+        }
+    }
+}
